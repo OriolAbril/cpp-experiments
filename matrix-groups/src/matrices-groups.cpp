9a2d10537efcbd91095d65a41858908f25a3ee25
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2019-11-08 00:58:32 +0100

    Restructure matrices-sel project
    Also made independent from R/Rcpp/RcppArmadillo

--- matrix-groups/src/matrices-groups.cpp
+++ matrix-groups/src/matrices-groups.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <random>
+#include <armadillo>
 #include <crossprodmat.h>
 
 int main() {
   int a=6;
 
-  std::cout << a;
+  std::cout << a << std::endl;
   return 0;
 }
